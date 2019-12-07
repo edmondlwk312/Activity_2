5e974f0f5669828b8394e481a59dccb4e8284c94
edmond <laiwk-wm17@student.tarc.edu.my>
2019-12-07 16:07:53 +0800

    Activity_2

--- app/src/main/java/com/example/activity_2/MainActivity.kt
+++ app/src/main/java/com/example/activity_2/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.activity_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.annotation.IntegerRes
+import android.widget.*
+import java.text.DecimalFormat
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity()
@@ -16,50 +14,78 @@ class MainActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editWeight = findViewById(R.id.editTextWeight) as EditText
-        val editHeight = findViewById(R.id.editTextHeight) as EditText
+        val editWeight = findViewById<EditText>(R.id.editTextWeight)
+        val editHeight = findViewById<EditText>(R.id.editTextHeight)
 
-        val viewBMI = findViewById(R.id.textViewBMI) as TextView
+        val viewBMI = findViewById<TextView>(R.id.textViewBMI)
+        val decimal = DecimalFormat("00.00")
 
-        val buttonForCalculate = findViewById(R.id.buttonCalculate) as Button
-        val buttonForReset = findViewById(R.id.buttonReset) as Button
+        val buttonForCalculate = findViewById<Button>(R.id.buttonCalculate)
+        val buttonForReset = findViewById<Button>(R.id.buttonReset)
+
+        val imageBMI = findViewById<ImageView>(R.id.imageViewProfile)
 
         buttonForCalculate.setOnClickListener(View.OnClickListener
         {
-            View -> viewBMI.text = "BMI: " + (weight().toDouble() / ((height().toDouble()/ 100).pow(2))).toDouble()
-            //View -> viewBMI.text = "BMI: " + (weight() / ((height() / 100) * (height() / 100))).toString()
+            val messageWeight:String = editWeight.text.toString()
+            val messageHeight:String = editHeight.text.toString()
 
-//            if(viewBMI.text < 20)
-//            {
-//                print("Not enough space")
-//            }
-//            else if(viewBMI.text > 20)
-//            {
-//                print("Pig")
-//            }
-//            else
-//            {
-//                print("shit")
-//            }
+            if((messageWeight.trim().isNotEmpty()) || (messageHeight.trim().isNotEmpty()))
+            {
+                if(messageWeight.trim().isNotEmpty())
+                {
+                    if(messageHeight.trim().isNotEmpty())
+                    {
+                        if((weight().toDouble() / ((height().toDouble()/ 100).pow(2))) < 18.5)
+                        {
+                            viewBMI.text = "BMI: " + decimal.format((weight() / ((height().toDouble()/ 100).pow(2)))) + "\n" + "Underweight"
+                            imageBMI.setImageResource(R.drawable.under)
+                        }
+                        else if(((weight().toDouble() / ((height().toDouble()/ 100).pow(2))) > 18.5) &&((weight().toDouble() / ((height().toDouble()/ 100).pow(2))) < 24.9))
+                        {
+                            viewBMI.text = "BMI: " + decimal.format((weight().toDouble() / ((height().toDouble()/ 100).pow(2)))) + "\n" + "Normal"
+                            imageBMI.setImageResource(R.drawable.normal)
+                        }
+                        else if((weight().toDouble() / ((height().toDouble()/ 100).pow(2))) > 25)
+                        {
+                            viewBMI.text = "BMI: " + decimal.format((weight().toDouble() / ((height().toDouble()/ 100).pow(2)))) + "\n" + "Overweight"
+                            imageBMI.setImageResource(R.drawable.over)
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(applicationContext, "Please Enter Height", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else
+                {
+                    Toast.makeText(applicationContext, "Please Enter Weight", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else
+            {
+                Toast.makeText(applicationContext, "Please Enter Weight and Height", Toast.LENGTH_SHORT).show()
+            }
         })
 
         buttonForReset.setOnClickListener(View.OnClickListener
         {
             View -> editHeight.getText().clear();
                     editWeight.getText().clear();
-                    viewBMI.text = "BMI: "
+                    imageBMI.setImageResource(R.drawable.empty)
+                    viewBMI.text = "BMI :"
         })
     }
 
     fun weight(): Int
     {
-        val input_weight = findViewById(R.id.editTextWeight) as EditText
+        val input_weight = findViewById<EditText>(R.id.editTextWeight)
         return Integer.parseInt(input_weight.text.toString())
     }
 
     fun height(): Int
     {
-        val input_height = findViewById(R.id.editTextHeight) as EditText
+        val input_height = findViewById<EditText>(R.id.editTextHeight)
         return Integer.parseInt(input_height.text.toString())
     }
 }
